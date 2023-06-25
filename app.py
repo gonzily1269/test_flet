0d9ff3f878594ed9c3707c16c4cb538ef5b241f4
gonzily1269 <gonzily1269@gmail.com>
2023-06-26 04:37:53 +0900

    盤の作成

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
+import os
 import flet as ft
+
+os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
 
 
 def main(page: ft.Page):
-    t = ft.Text(value="Hello, world!", color="green")
-    page.controls.append(t)
-    page.update()
+    for i in range(5):
+        r = ft.Row(wrap=5, expand=False)
+        page.add(r)
+        for j in range(5):
+            r.controls.append(
+                ft.Container(
+                    content=ft.Text(f"Item {i}-{j}"),
+                    width=100,
+                    height=100,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.AMBER_100,
+                    border=ft.border.all(1, ft.colors.AMBER_400),
+                    border_radius=ft.border_radius.all(5),
+                    ink=True,
+                    on_click=lambda e: print(f"Item clicked!"),
+                )
+            )
+            page.update()
 
 
-ft.app(target=main)
+ft.app(target=main, view=ft.WEB_BROWSER)
