25fc4899c5cac0c08b5077314fa433d820571da5
gonzily1269 <gonzily1269@gmail.com>
2023-06-28 01:29:21 +0900

    追加：値と色の変更

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -1,46 +1,58 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.title = "Drag and Drop example"
 
     def drag_accept(e):
-        # get draggable (source) control by its ID
+        # IDからドラッグ対象のsourceを持ってくる
         src = page.get_control(e.src_id)
-        # update text inside draggable control
-        src.content.content.value = "0"
-        # update text inside drag target control
-        e.control.content.content.value = "1"
+
+        # ドラッグ側の値と色を取得→ターゲットの値と色と交換
+        tmp_src_value = src.content.content.value
+        src.content.content.value = e.control.content.content.content.value
+        tmp_src_color = src.content.bgcolor
+        src.content.bgcolor = e.control.content.content.bgcolor
+        e.control.content.content.content.value = tmp_src_value
+        e.control.content.content.bgcolor = tmp_src_color
         page.update()
 
     page.add(
         ft.Row(
             [
-                ft.Draggable(
-                    group="number",
-                    content=ft.Container(
-                        width=50,
-                        height=50,
-                        bgcolor=ft.colors.CYAN_200,
-                        border_radius=5,
-                        content=ft.Text("1", size=20),
-                        alignment=ft.alignment.center,
-                    ),
-                ),
-                ft.Container(width=100),
                 ft.DragTarget(
                     group="number",
-                    content=ft.Container(
-                        width=50,
-                        height=50,
-                        bgcolor=ft.colors.PINK_200,
-                        border_radius=5,
-                        content=ft.Text("0", size=20),
-                        alignment=ft.alignment.center,
+                    content=ft.Draggable(
+                        group="number",
+                        content=ft.Container(
+                            width=50,
+                            height=50,
+                            bgcolor=ft.colors.CYAN_200,
+                            border_radius=5,
+                            content=ft.Text("1", size=20),
+                            alignment=ft.alignment.center,
+                        ),
+                    ),
+                    on_accept=drag_accept,
+                ),
+                ft.DragTarget(
+                    group="number",
+                    content=ft.Draggable(
+                        group="number",
+                        content=ft.Container(
+                            width=50,
+                            height=50,
+                            bgcolor=ft.colors.PINK_200,
+                            border_radius=5,
+                            content=ft.Text("0", size=20),
+                            alignment=ft.alignment.center,
+                        ),
                     ),
                     on_accept=drag_accept,
                 ),
             ]
         )
     )
+
 
 ft.app(target=main)
